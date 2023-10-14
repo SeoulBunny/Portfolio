71a871ab0fcfefa696c5034a759a388f30ae78d8
Joe <globaljoe4@gmail.com>
2023-10-14 13:52:48 +0900

    first revision

--- script-main.js
+++ script-main.js
@@ -1,36 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const portfolioImage = document.querySelector('.portfolio');
-    const aboutImage = document.querySelector('.about');
-    
 
-    function addHoverEffect(element) {
-        element.addEventListener('mouseover', function() {
-            element.style.boxShadow = '0 0 100px rgba(250, 250, 15, 50)';
-        });
 
-        element.addEventListener('mouseout', function() {
-            element.style.boxShadow = 'none';
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the image and text elements
+    const portraitImage = document.getElementById("portrait");
+    const portraitText = document.getElementById("portrait-text");
 
-    addHoverEffect(portfolioImage);
-    addHoverEffect(aboutImage);
+    // Add mouseover event listener
+    portraitImage.addEventListener("mouseover", function () {
+        // Hide the image
+        portraitImage.style.opacity = 0;
+        // Show the text
+        portraitText.style.display = "block";
+    });
+
+    // Add mouseout event listener
+    portraitText.addEventListener("mouseout", function () {
+        // Hide the text
+        portraitText.style.display = "none";
+        // Show the image
+        portraitImage.style.opacity = 1;
+    });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const portraitImage = document.querySelector('.portrait');
-    
-    
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    function addHoverEffect(element) {
-        element.addEventListener('mouseover', function() {
-            element.style.boxShadow = '0 0 250px rgba(155, 55, 20, 90)';
-        });
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-        element.addEventListener('mouseout', function() {
-            element.style.boxShadow = 'none';
-        });
-    }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    addHoverEffect(portraitImage);
-});
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
